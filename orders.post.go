81b2e2392b45f1aabc0499dcc461905a4f418b8a
tylim <limteikyee@gmail.com>
2026-01-27 23:30:54 +0800

    done sending data to pending channel

--- orders.post.go
+++ orders.post.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"net/http"
+	"sync"
 	"time"
 
 	"github.com/gin-gonic/gin"
@@ -12,28 +13,45 @@ type ordersPostBody struct {
 }
 
 var id_robot_latest = 0
+var mu sync.Mutex
 
 func ordersPost() {
 
 	r.POST("/order", func(c *gin.Context) {
 		var body ordersPostBody
+		var newOrder Pending
 
 		if err := c.ShouldBindJSON(&body); err != nil {
 			c.JSON(400, gin.H{"error": "Invalid JSON provided!"})
 			return
 		}
-		newOrder := Pending{
-			Id_order:    id_robot_latest + 1,
-			Time_create: time.Now(),
-		}
-		if body.Type == "vip" {
-			pending.Vip = append(pending.Vip, newOrder)
 
-		} else if body.Type == "regular" {
-			pending.Regular = append(pending.Regular, newOrder)
-		} else {
-			c.JSON(400, gin.H{"error": "Invalid type provided!"})
-			return
+		mu.Lock()
+		defer mu.Unlock()
+
+		func() {
+
+			id_robot_latest++
+			newOrder = Pending{
+				Id_order:    id_robot_latest,
+				Time_create: time.Now(),
+			}
+			switch body.Type {
+			case "vip":
+				pending.Vip = append(pending.Vip, newOrder)
+			case "regular":
+				pending.Regular = append(pending.Regular, newOrder)
+			default:
+				c.JSON(400, gin.H{"error": "Invalid type provided!"})
+				return
+			}
+		}()
+
+		pendingChan <- PendingResponse{
+			Pending: newOrder,
+			Type:    body.Type,
+			Queue:   "pending",
+			Action:  "add",
 		}
 
 		c.JSON(http.StatusOK, gin.H{})
